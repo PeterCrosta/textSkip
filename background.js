2b0bf5f4792452a091b089553f10566ec88578d3
PeterCrosta <pscrosta@gmail.com>
2020-12-01 17:37:07 -0800

    new resource, trying again

--- background.js
+++ background.js
@@ -3,17 +3,20 @@
 // found in the LICENSE file.
 
 'use strict';
-console.log(chrome)
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+// chrome.runtime.onInstalled.addListener(function() {
+  chrome.runtime.onInstalled.addListener(function request(request, sender, sendResponse) {
+    console.log('in runtime')
+  })
+  // chrome.storage.sync.set({color: '#3aa757'}, function() {
+  //   console.log('The color is green.');
+  // });
+  console.log('in background')
+  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  //   chrome.declarativeContent.onPageChanged.addRules([{
+  //     conditions: [new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostEquals: 'developer.chrome.com'},
+  //     })],
+  //     actions: [new chrome.declarativeContent.ShowPageAction()]
+  //   }]);
+  // });
+// });
--- finder.js
+++ finder.js
@@ -1,13 +1,31 @@
-const keyWords = ['cup', 'cups', 'tsp', 'tbsp', 'teaspoon', 'teaspoons', 'tablespoon','tablespoons', 'gram', 'grams', 'litre','litres','milliliter', 'millilitres']
-let lists = [...document.getElementsByTagName('li')]
-let item = lists.find(el => {
-  for (let i = 0; i < keyWords.length; i++) {
-      if (el.innerText.includes(keyWords[i])) return true
-  }
-})
-while (true) {
-    if (item.nodeName !== 'UL') item = item.parentElement
-    else break
-}
+const finder = document.getElementById('find')
 
-item.scrollIntoView()
+finder.onclick = function() {
+    console.log('in the on click')
+    const keyWords = ['cup', 'cups', 'tsp', 'tbsp', 'teaspoon', 'teaspoons', 'tablespoon','tablespoons', 'gram', 'grams', 'litre','litres','milliliter', 'millilitres']
+    let lists = [...document.getElementsByTagName('li')]
+    let item = lists.find(el => {
+      for (let i = 0; i < keyWords.length; i++) {
+          if (el.innerText.includes(keyWords[i])) return true
+      }
+    })
+    while (true) {
+        if (item.nodeName !== 'UL') item = item.parentElement
+        else break
+    }
+    
+    item.scrollIntoView({ behavior: 'smooth', block: 'center'})    
+}
+// const keyWords = ['cup', 'cups', 'tsp', 'tbsp', 'teaspoon', 'teaspoons', 'tablespoon','tablespoons', 'gram', 'grams', 'litre','litres','milliliter', 'millilitres']
+// let lists = [...document.getElementsByTagName('li')]
+// let item = lists.find(el => {
+//   for (let i = 0; i < keyWords.length; i++) {
+//       if (el.innerText.includes(keyWords[i])) return true
+//   }
+// })
+// while (true) {
+//     if (item.nodeName !== 'UL') item = item.parentElement
+//     else break
+// }
+
+// item.scrollIntoView({ behavior: 'smooth', block: 'center'})
