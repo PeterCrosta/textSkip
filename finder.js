dc1c98cd579e2de9114d72a884485bf63fc3648e
PeterCrosta <pscrosta@gmail.com>
2021-04-13 15:18:50 -0700

    bit of cleanup

--- finder.js
+++ finder.js
@@ -1,22 +1,48 @@
-const finder = document.getElementById('find')
+// THESE ARE TRIAL ATTEMPTS TO GET IT WORKING
 
 
-finder.onclick = function() {
-    const bodies = document.getElementsByTagName('body')
-    console.log('in the right function')
-    let body = bodies[0], searching = true;
-    console.log('is body? ', body.nodeName)
-    const findIngredients = (tag = body) => {
-        console.log('in recursion: ', tag.nodeName)
-        if (!searching) return
-        for (let i = 0; i < tag.children.length; i++) {
-            const child = tag.children[i]
 
-        }
-    }
-    findIngredients()
-}
 
+
+
+
+// const finder = document.getElementById('find')
+
+
+// finder.onclick = function() {
+//     const bodies = document.getElementsByTagName('body')
+//     console.log('in the right function')
+//     let body = bodies[0], searching = true;
+//     console.log('is body? ', body.nodeName)
+//     const findIngredients = (tag = body) => {
+//         console.log('in recursion: ', tag.nodeName)
+//         if (!searching) return
+//         for (let i = 0; i < tag.children.length; i++) {
+//             const child = tag.children[i]
+
+//         }
+//     }
+//     findIngredients()
+// }
+
+// const finder = () => {
+//     console.log('now youre in the function')
+//     const keyWords = ['cup', 'cups', 'tsp', 'tbsp', 'teaspoon', 'teaspoons', 'tablespoon','tablespoons', 'grams', 'litre','litres','milliliter', 'millilitres', 'degree', '°']
+//     let lists = [...document.getElementsByTagName('li')]
+//     let item = lists.find(el => {
+//       // console.log('in find: ', el.innerText)
+//       for (let i = 0; i < keyWords.length; i++) {
+//           if (el.innerText.includes(keyWords[i])) return true
+//       }
+//     })
+//     console.log('item: ',item.innerText)
+//     while (true) {
+//         if (item.nodeName !== 'UL' && item.nodeName !== 'OL') item = item.parentElement
+//         else break
+//     }
+    
+//     item.scrollIntoView({ behavior: 'smooth', block: 'center'})    
+// }
 
 // finder.onclick = function() {
 //     console.log('in the on click')
--- popup.js
+++ popup.js
@@ -1,22 +1,3 @@
-// const finder = () => {
-//     console.log('now youre in the function')
-//     const keyWords = ['cup', 'cups', 'tsp', 'tbsp', 'teaspoon', 'teaspoons', 'tablespoon','tablespoons', 'grams', 'litre','litres','milliliter', 'millilitres', 'degree', '°']
-//     let lists = [...document.getElementsByTagName('li')]
-//     let item = lists.find(el => {
-//       // console.log('in find: ', el.innerText)
-//       for (let i = 0; i < keyWords.length; i++) {
-//           if (el.innerText.includes(keyWords[i])) return true
-//       }
-//     })
-//     console.log('item: ',item.innerText)
-//     while (true) {
-//         if (item.nodeName !== 'UL' && item.nodeName !== 'OL') item = item.parentElement
-//         else break
-//     }
-    
-//     item.scrollIntoView({ behavior: 'smooth', block: 'center'})    
-// }
-
 const finder = () => {
     const divs = document.getElementsByTagName('div')
     const lis = document.getElementsByTagName('li')
