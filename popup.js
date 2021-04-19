61562f6be82f5517339cc920a4ee5fb8d97aa531
PeterCrosta <pscrosta@gmail.com>
2021-04-19 14:53:08 -0700

    working without popup, new png added

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 const finder = () => {
+    console.log('in finder func')
     const divs = document.getElementsByTagName('div')
     const lis = document.getElementsByTagName('li')
-    const tags = [...lis,...divs]
+    const spans = document.getElementsByTagName('span')
+    const tags = [...lis,...divs,...spans]
     for (let tag of tags) {
         const classStr = tag.className
         if (classStr.includes('Ingredient') || classStr.includes('ingredient')) {
@@ -10,3 +12,15 @@ const finder = () => {
     }
 }
 finder()
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('in dom loaded')
+//     document.getElementById('find').addEventListener('click', finder)
+// })
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('find').addEventListener('click', finder);
+// });
+
+// const button = document.getElementById('find')
+// console.log('button: ', button)
+// button.addEventListener('click', finder)
